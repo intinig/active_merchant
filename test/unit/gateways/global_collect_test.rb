1d2a32f66f70b2808a496c4582d8c623d3645809
Andrea Franz <andrea@gravityblast.com>
2009-01-15 14:55:49 +0100

    fixed unit tests

--- test/unit/gateways/global_collect_test.rb
+++ test/unit/gateways/global_collect_test.rb
@@ -66,19 +66,6 @@ class GlobalCollectTest < Test::Unit::TestCase
     assert response.test?
   end
   
-  def test_successful_authorize_with_fraud_code
-    gateway = GlobalCollectGateway.new( :merchant => '1', :ip => '123.123.123.123', :test => true, :secure_3d => true)
-    gateway.expects(:ssl_post).at_least(2).returns(successful_insert_order_with_payment_and_fraud_code_response, successful_do_checkenrollment_response)
-    assert response = gateway.authorize(@amount, @credit_card, @options)
-    
-    assert_instance_of Response, response
-    assert_success response
-    
-    # Replace with authorization number from the successful response
-    assert_equal '123', response.authorization
-    assert response.test?
-  end
-  
   def test_authorize_should_not_call_do_checkenrollment_if_secure_3d_is_false
     @gateway.expects(:ssl_post).returns(successful_insert_order_with_payment_response)
     assert response = @gateway.authorize(@amount, @credit_card, @options)
@@ -368,11 +355,14 @@ class GlobalCollectTest < Test::Unit::TestCase
         </META>
         <PARAMS>
           <PAYMENT>
-            <ORDERID>9998990013</ORDERID>            
+            <CURRENCYCODE>EUR</CURRENCYCODE>
+            <COUNTRYCODE>NL</COUNTRYCODE>
+            <ORDERID>9998990013</ORDERID>
             <EXPIRYDATE>1206</EXPIRYDATE>
             <CREDITCARDNUMBER>4567350000427977</CREDITCARDNUMBER>
             <CURRENCYCODE>EUR</CURRENCYCODE>            
-            <AMOUNT>29990</AMOUNT>
+            <AMOUNT>29990</AMOUNT>            
+            <AUTHENTICATIONINDICATOR>1</AUTHENTICATIONINDICATOR>
           </PAYMENT>
         </PARAMS>
       </REQUEST>
@@ -515,62 +505,7 @@ class GlobalCollectTest < Test::Unit::TestCase
       </REQUEST>
     </XML>
     XML
-  end
-  
-  def successful_insert_order_with_payment_and_fraud_code_response
-    <<-XML
-    <XML>
-      <REQUEST>
-        <ACTION>INSERT_ORDERWITHPAYMENT</ACTION>
-        <META>
-          <MERCHANTID>1</MERCHANTID>
-          <IPADDRESS>123.123.123.123</IPADDRESS>
-          <VERSION>1.0</VERSION>
-          <REQUESTIPADDRESS>123.123.123.123</REQUESTIPADDRESS>
-        </META>
-        <PARAMS>
-          <ORDER>
-            <ORDERID>9998990013</ORDERID>
-            <MERCHANTREFERENCE>9998990013</MERCHANTREFERENCE>
-            <AMOUNT>29990</AMOUNT>
-            <CURRENCYCODE>EUR</CURRENCYCODE>
-            <COUNTRYCODE>NL</COUNTRYCODE>
-            <LANGUAGECODE>nl</LANGUAGECODE>
-          </ORDER>
-          <PAYMENT>
-            <PAYMENTPRODUCTID>1</PAYMENTPRODUCTID>
-            <AMOUNT>2345</AMOUNT>
-            <CURRENCYCODE>EUR</CURRENCYCODE>
-            <CREDITCARDNUMBER>4567350000427977</CREDITCARDNUMBER>
-            <EXPIRYDATE>1206</EXPIRYDATE>
-            <COUNTRYCODE>NL</COUNTRYCODE>
-            <LANGUAGECODE>nl</LANGUAGECODE>
-          </PAYMENT>
-        </PARAMS>
-        <RESPONSE>
-          <RESULT>OK</RESULT>
-          <META>
-            <RESPONSEDATETIME>20040718145902</RESPONSEDATETIME>
-            <REQUESTID>245</REQUESTID>
-          </META>
-          <ROW>
-            <MERCHANTID>1</MERCHANTID>
-            <ORDERID>9998990013</ORDERID>
-            <EFFORTID>1</EFFORTID>
-            <ATTEMPTID>1</ATTEMPTID>
-            <STATUSID>800</STATUSID>
-            <STATUSDATE>20030829171416</STATUSDATE>
-            <PAYMENTREFERENCE>185800005380</PAYMENTREFERENCE>
-            <ADDITIONALREFERENCE>19998990013</ADDITIONALREFERENCE>
-            <AUTHORISATIONCODE>123</AUTHORISATIONCODE>
-            <FRAUDRESULT>C</FRAUDRESULT>
-            <FRAUDCODE>0100</FRAUDCODE>     
-          </ROW>
-        </RESPONSE>
-      </REQUEST>
-    </XML>
-    XML
-  end  
+  end      
 
   def successful_do_checkenrollment_response
     <<-XML
